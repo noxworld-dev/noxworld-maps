4c6b73d2e55a1272328dc8b3dae00750648e590d
Denys Smirnov <dennwc@pm.me>
2024-02-09 11:38:20 +0200

    Test account data encoding.

---
+++ crossroad/accountdata_test.go
@@ -0,0 +1,58 @@
+package noxworld
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/noxworld-dev/noxscript/ns/v4"
+	_ "github.com/noxworld-dev/noxscript/ns/v4/test"
+)
+
+func TestAccountData(t *testing.T) {
+	t.Run("json", func(t *testing.T) {
+		data, err := json.Marshal(MyAccountData{})
+		if err != nil {
+			t.Fatal(err)
+		}
+		var v MyAccountData
+		if err = json.Unmarshal(data, &v); err != nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("unexported", func(t *testing.T) {
+		checkUnexported(t, reflect.TypeOf(MyAccountData{}), "")
+	})
+	t.Run("save-and-load", func(t *testing.T) {
+		pl := ns.HostPlayer()
+
+		data := loadMyQuestData(pl)
+		data.Character.Name = "Test"
+		data.Quest.JoinTheFireKnights = true
+		saveMyQuestData(pl, data)
+
+		data2 := loadMyQuestData(pl)
+		if data != data2 {
+			t.Fatal("account save failed")
+		}
+	})
+}
+
+func checkUnexported(t testing.TB, rt reflect.Type, pref string) {
+	switch rt.Kind() {
+	case reflect.Pointer:
+		checkUnexported(t, rt.Elem(), pref)
+	case reflect.Struct:
+		for i := 0; i < rt.NumField(); i++ {
+			f := rt.Field(i)
+			if !f.IsExported() {
+				name := rt.Name()
+				if name == "" {
+					name = pref
+				}
+				t.Errorf("unexported field: %s.%s", name, f.Name)
+			}
+			checkUnexported(t, f.Type, f.Name)
+		}
+	}
+}
--- crossroad/go.mod
+++ crossroad/go.mod
@@ -4,6 +4,9 @@ go 1.21
 
 toolchain go1.21.6
 
-require github.com/noxworld-dev/noxscript/ns/v4 v4.17.0
+require (
+	github.com/noxworld-dev/noxscript/ns/v4 v4.18.0
+	github.com/noxworld-dev/opennox-lib v0.0.0-20240128132430-629f4ef030f1
+)
 
-require github.com/noxworld-dev/opennox-lib v0.0.0-20240128132430-629f4ef030f1
+require golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
